9c4a5dc57cc86b1ce4fb144547d8e2accd45c172
arpit agrawal <agrawalarpit1799@gmail.com>
2025-08-22 04:26:09 +0530

    Fix crypto encryption: use createCipheriv instead of deprecated createCipher

--- src/services/authService.js
+++ src/services/authService.js
@@ -11,7 +11,7 @@ class AuthService {
     const algorithm = 'aes-256-cbc';
     const key = crypto.scryptSync(process.env.ENCRYPTION_KEY, 'salt', 32);
     const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipher(algorithm, key);
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
     let encrypted = cipher.update(text, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     return iv.toString('hex') + ':' + encrypted;
@@ -28,7 +28,7 @@ class AuthService {
     const parts = encryptedText.split(':');
     const iv = Buffer.from(parts[0], 'hex');
     const encrypted = parts[1];
-    const decipher = crypto.createDecipher(algorithm, key);
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
     let decrypted = decipher.update(encrypted, 'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
